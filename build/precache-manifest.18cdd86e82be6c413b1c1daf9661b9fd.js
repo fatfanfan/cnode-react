570b54c15cd1cb88b32d1443fa8038d83992f749
fanfan <fatfan1020@gmail.com>
2020-07-18 19:15:25 +0800

    打包文件上传

---
+++ build/precache-manifest.18cdd86e82be6c413b1c1daf9661b9fd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa2a743bab7ca58ac20494155d32f6b8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5f6e6286e4fcef742fc8",
+    "url": "./static/css/2.7363a994.chunk.css"
+  },
+  {
+    "revision": "1e7bde59847e19fc6155",
+    "url": "./static/css/main.f3e5d197.chunk.css"
+  },
+  {
+    "revision": "5f6e6286e4fcef742fc8",
+    "url": "./static/js/2.0bb759d1.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "./static/js/2.0bb759d1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1e7bde59847e19fc6155",
+    "url": "./static/js/main.f9f3f010.chunk.js"
+  },
+  {
+    "revision": "4598f2f8a64a8adc746e",
+    "url": "./static/js/runtime-main.9b265669.js"
+  }
+]);
